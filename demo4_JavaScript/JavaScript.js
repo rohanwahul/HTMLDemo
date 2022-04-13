628215bae624d6d457cb0f54557fefc272118f9e
rohanwahul <rohanwahul@gmail.com>
2022-04-13 15:37:04 -0700

    new

---
+++ demo4_JavaScript/JavaScript.js
@@ -0,0 +1,37 @@
+﻿function add() {
+    var num1 = document.getElementById("n1").value;
+    var num2 = document.getElementById("n2").value;
+
+    var sum = parseInt(num1) + parseInt(num2);
+
+    //console.log(sum);
+
+    document.getElementById("n3").value = sum;
+
+}
+
+function mul() {
+    var num1 = document.getElementById("n1").value;
+    var num2 = document.getElementById("n2").value;
+
+    var mul = parseInt(num1) * parseInt(num2);
+
+   // console.log(mul);
+
+    document.getElementById("n3").value = mul;
+
+}
+
+
+function div() {
+    var num1 = document.getElementById("n1").value;
+    var num2 = document.getElementById("n2").value;
+
+    var div = parseInt(num1) / parseInt(num2);
+    
+   // console.log(div);
+
+    document.getElementById("n3").value = div;
+
+}
+
